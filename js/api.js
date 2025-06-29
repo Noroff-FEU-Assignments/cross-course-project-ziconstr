3c6fa3d2d7986bf838c9b4802e8baaa4be13d4d9
ziconstr <76712091+ziconstr@users.noreply.github.com>
2025-06-29 23:02:46 +0200

    Fix: dynamic single product fetch, clean API usage

--- js/api.js
+++ js/api.js
@@ -3,34 +3,19 @@ import { loader } from "./loader.js";
 
 const ownAPI = "https://www.nadstarr.com/wp-json/wc/store/products";
 
-export async function fetchmovies() {
-  loader();
-  const response = await fetch(ownAPI);
-
-  const result = await response.json();
-
-  if (response.ok) {
-    return result;
-  }
-
-  throw new Error("Try again later!");
-}
-
-
-const queryString = document.location.search;
-
-const params = new URLSearchParams(queryString);
-
-const id = params.get("id");
-
-export const title = params.get("title");
-
-const specificmovieUrl = ownAPI + "/" + id;
-
 export async function fetchmovie() {
   loader();
-  const response = await fetch(specificmovieUrl);
+  const queryString = document.location.search;
+  const params = new URLSearchParams(queryString);
+  const id = params.get("id");
 
+  if (!id) {
+    throw new Error("No ID found in query string");
+  }
+
+  const specificmovieUrl = ownAPI + "/" + id;
+
+  const response = await fetch(specificmovieUrl);
   const movieDetails = await response.json();
 
   return movieDetails;
