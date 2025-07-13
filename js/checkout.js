11ac3c13565b154c7296fc2e40c31cacbe0caaf8
ziconstr <76712091+ziconstr@users.noreply.github.com>
2025-07-13 20:35:14 +0200

    m

--- js/checkout.js
+++ js/checkout.js
@@ -4,7 +4,7 @@ import { clearCart } from "./storage.js";
 
 document.getElementById("checkout-form").addEventListener("submit", function (e) {
   e.preventDefault();
-  // In real project you would validate payment, but for CA we simulate success
+
   clearCart();
   window.location.href = "thankyou.html";
 });
